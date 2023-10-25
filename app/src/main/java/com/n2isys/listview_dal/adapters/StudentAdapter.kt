c44b88c4d6d3d9879a037d0f43c1645421dccfc8
Daljung <msfall@hanmail.net>
2023-10-25 19:24:16 +0900

    adepter 작업 마무리

--- app/src/main/java/com/n2isys/listview_dal/adapters/StudentAdapter.kt
+++ app/src/main/java/com/n2isys/listview_dal/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.n2isys.listview_dal.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.n2isys.listview_dal.R
 import com.n2isys.listview_dal.datas.Student
 
 class StudentAdapter(
@@ -9,5 +13,17 @@ class StudentAdapter(
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student> (mContext,resId,mList){
 
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
 
 }
