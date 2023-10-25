e190cdbf5bcfe3de4ec7b77215c6ae847468d0d1
Daljung <msfall@hanmail.net>
2023-10-25 19:58:43 +0900

    학생목록에 arraylist 생성 , 실제 목록 추가

--- app/src/main/java/com/n2isys/listview_dal/MainActivity.kt
+++ app/src/main/java/com/n2isys/listview_dal/MainActivity.kt
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import com.n2isys.listview_dal.databinding.ActivityMainBinding
+import com.n2isys.listview_dal.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val viewBind = ActivityMainBinding.inflate(layoutInflater,null,false)
 
         setContentView(viewBind.root)
+
+        mStudentList.add( Student("홍기봉",1968))
+        mStudentList.add( Student("정달호",1978))
+        mStudentList.add( Student("소대연",1988))
+        mStudentList.add( Student("유필성",1985))
+        mStudentList.add( Student("김주하",1978))
+        mStudentList.add( Student("김미나",1984))
+        mStudentList.add( Student("한인택",1991))
 
 
     }
